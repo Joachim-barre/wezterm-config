98238d3f0c5d7aabbff02f291e7045d731d22c9f
Joachim-barre <joachim.barre22@laposte.net>
2024-04-21 11:21:37 +0200

    modified keybinds

--- keys.lua
+++ keys.lua
@@ -2,11 +2,42 @@ local M = {}
 
 function M.keys()
     local wezterm = require('wezterm')
+    act = wezterm.action
     return {
         {
             key = 'r',
             mods = 'LEADER',
-            action = wezterm.action.ReloadConfiguration,
+            action = act.ReloadConfiguration,
+        },
+        {
+            key = 's',
+            mods = 'LEADER',
+            action = act.SplitVertical { domain = 'CurrentPaneDomain' }
+        },
+        {
+            key = 'v',
+            mods = 'LEADER',
+            action = act.SplitHorizontal { domain = 'CurrentPaneDomain' }
+        },
+        {
+            key = 'LeftArrow',
+            mods = 'LEADER',
+            action = act.ActivatePaneDirection 'Left',
+        },
+        {
+            key = 'RightArrow',
+            mods = 'LEADER',
+            action = act.ActivatePaneDirection 'Right',
+        },
+        {
+            key = 'UpArrow',
+            mods = 'LEADER',
+            action = act.ActivatePaneDirection 'Up',
+        },
+        {
+            key = 'DownArrow',
+            mods = 'LEADER',
+            action = act.ActivatePaneDirection 'Down',
         },
     }
 end
--- wezterm.lua
+++ wezterm.lua
@@ -4,7 +4,7 @@ local config = wezterm.config_builder()
 
 config.font = wezterm.font 'Hack'
 config.color_scheme = 'Gnometerm (terminal.sexy)'
-config.leader = { key = 'b', mods = 'CTRL', timeout_milliseconds = 1000 }
+config.leader = { key = 'x', mods = 'CTRL', timeout_milliseconds = 1000 }
 config.keys = require('keys').keys()
 
 config.colors = {
