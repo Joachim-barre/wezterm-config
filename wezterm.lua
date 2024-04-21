d5b23f5ca99015395669c26864b82d897e926b21
Joachim-barre <joachim.barre22@laposte.net>
2024-04-21 11:44:34 +0200

    remodfied font size

--- wezterm.lua
+++ wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.font = wezterm.font 'Hack'
-config.font_size = 11.5
+config.font_size = 11.0
 config.color_scheme = 'Gnometerm (terminal.sexy)'
 config.leader = { key = 'x', mods = 'CTRL', timeout_milliseconds = 1000 }
 config.keys = require('keys').keys()
