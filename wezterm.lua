b4ad4d117e4b1df8aab824800bde3392e4733e20
Joachim-barre <joachim.barre22@laposte.net>
2024-04-21 11:09:19 +0200

    changed background

--- wezterm.lua
+++ wezterm.lua
@@ -2,9 +2,13 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.font = wezterm.font 'JetBrains Mono'
+config.font = wezterm.font 'Hack'
 config.color_scheme = 'Gnometerm (terminal.sexy)'
 config.leader = { key = 'b', mods = 'CTRL', timeout_milliseconds = 1000 }
 config.keys = require('keys').keys()
+
+config.colors = {
+    background = ' #151515 ',
+}
 
 return config
