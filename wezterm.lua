7b638432cb0e13c2e011f46ad376d929b80fefe9
Joachim-barre <joachim.barre22@laposte.net>
2024-04-21 11:05:06 +0200

    fixed colorscheme

--- wezterm.lua
+++ wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.font = wezterm.font 'JetBrains Mono'
-config.color_scheme = 'Gnometerm'
+config.color_scheme = 'Gnometerm (terminal.sexy)'
 config.leader = { key = 'b', mods = 'CTRL', timeout_milliseconds = 1000 }
 config.keys = require('keys').keys()
 
