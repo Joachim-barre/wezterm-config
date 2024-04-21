4512c88ad50dc6d5acfbc409d50dac036b2793d1
Joachim-barre <joachim.barre22@laposte.net>
2024-04-21 12:11:14 +0200

    modified theme

--- wezterm.lua
+++ wezterm.lua
@@ -4,12 +4,8 @@ local config = wezterm.config_builder()
 
 config.font = wezterm.font 'Hack'
 config.font_size = 11.0
-config.color_scheme = 'Gnometerm (terminal.sexy)'
+config.color_scheme = 'Kgx'
 config.leader = { key = 'x', mods = 'CTRL', timeout_milliseconds = 1000 }
 config.keys = require('keys').keys()
-
-config.colors = {
-    background = ' #151515 ',
-}
 
 return config
