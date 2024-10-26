2af6a545a9164335ede2414c97731cac68c8cb7a
Joachim-barre <joachim.barre22@laposte.net>
2024-10-26 16:24:50 +0200

    updated leader key

--- wezterm.lua
+++ wezterm.lua
@@ -5,7 +5,7 @@ local config = wezterm.config_builder()
 config.font = wezterm.font 'Hack'
 config.font_size = 11.0
 config.color_scheme = 'Kgx'
-config.leader = { key = 'x', mods = 'CTRL', timeout_milliseconds = 1000 }
+config.leader = { key = 'q', mods = 'CTRL', timeout_milliseconds = 1000 }
 config.keys = require('keys').keys()
 
 return config
